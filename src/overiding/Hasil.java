c52b59209d3034996432fbd03876af865dadf729
nabilafrhn <nabilafarahana01@gmail.com>
2022-03-28 17:35:50 +0700

    yeay

---
+++ src/overiding/Hasil.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overiding;
+
+/**
+ *
+ * @author ASUS VIVOBOOK 14
+ */
+public class Hasil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        BangunDatar bangundatar=new BangunDatar();
+        
+        Persegi Persegi = new Persegi();
+        Persegi.sisi=8;
+        
+        PersegiPanjang perjang =new PersegiPanjang();
+        perjang.lebar=11;
+        perjang.panjang=2;
+        
+        
+       bangundatar.luas();
+       bangundatar.keliling();
+       
+       Persegi.luas();
+       Persegi.keliling();
+       
+       perjang.luas();
+       perjang.keliling();
+        
+    }
+    
+
+    }
+    
+
